d4848076400852f723338cd67894c7a7ca3f368b
SanjayReddy3015 <sanjayreddydhubba@gmail.com>
2026-02-19 20:34:07 +0530

    Update init.py

--- app/api/init.py
+++ app/api/init.py
@@ -1,1 +1,11 @@
 
+# wikikisan-backend/app/api/__init__.py
+
+"""
+WikiKisan API Routes
+This package contains all the endpoint definitions.
+"""
+
+# This allows you to import routers more easily in main.py
+# Example: from app.api import community
+from . import community

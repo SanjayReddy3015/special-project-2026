3bf6d959f77e82a5aeb55528bf55d67af71ac09d
SanjayReddy3015 <sanjayreddydhubba@gmail.com>
2026-02-19 20:31:04 +0530

    Update init.py

--- app/init.py
+++ app/init.py
@@ -1,1 +1,9 @@
+# wikikisan-backend/app/__init__.py
 
+"""
+WikiKisan Backend Package
+This file marks the 'app' directory as a Python package.
+"""
+
+__version__ = "1.0.0"
+__author__ = "WikiKisan Team"
